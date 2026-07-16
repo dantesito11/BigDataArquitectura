6bf96301db596386a1a2709a1707d942f838b9fd
vanessagironda <vanessa.gironda@gmail.com>
2026-07-16 16:04:05 -0400

    Update script_sqoop_textfile.sh

--- datanode/scripts/sqoop/script_sqoop_textfile.sh
+++ datanode/scripts/sqoop/script_sqoop_textfile.sh
@@ -54,12 +54,12 @@ sqoop import \
 --target-dir=/user/datapath/datasets/orders \
 --delete-target-dir > /tmp/log_orders.log
 
-sqoop import \
---connect "jdbc:mysql://mysql:3306/bd_vanessa" \
---username=root \
---password=root \
---table student_mat \
---split-by age \
---as-textfile \
---target-dir=/user/raw/mysql/bd_vanessa/t_student_mat \
---delete-target-dir > /tmp/log_customer.log
+# sqoop import \
+# --connect "jdbc:mysql://mysql:3306/bd_vanessa" \
+# --username=root \
+# --password=root \
+# --table student_mat \
+# --split-by age \
+# --as-textfile \
+# --target-dir=/user/raw/mysql/bd_vanessa/t_student_mat \
+# --delete-target-dir > /tmp/log_customer.log
