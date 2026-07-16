e404f51324f6aa39d6a9c679add6dc05410e499d
vanessagironda <vanessa.gironda@gmail.com>
2026-07-16 16:53:53 -0400

    Add Sqoop script to import data from MySQL

---
+++ datanode/scripts/sqoop/script_sqoop_textfile.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+sqoop import \
+--connect "jdbc:mysql://mysql:3306/retail_db" \
+--username=root \
+--password=root \
+--table order_items \
+--as-textfile \
+--target-dir=/user/datapath/datasets/order_items \
+--delete-target-dir > /tmp/log_order_items.log
+
+sqoop import \
+--connect "jdbc:mysql://mysql:3306/retail_db" \
+--username=root \
+--password=root \
+--table products \
+--as-textfile \
+--target-dir=/user/datapath/datasets/products \
+--delete-target-dir > /tmp/log_products.log
+
+sqoop import \
+--connect "jdbc:mysql://mysql:3306/retail_db" \
+--username=root \
+--password=root \
+--table customers \
+--as-textfile \
+--target-dir=/user/datapath/datasets/customers \
+--delete-target-dir > /tmp/log_customer.log
+
+sqoop import \
+--connect "jdbc:mysql://mysql:3306/retail_db" \
+--username=root \
+--password=root \
+--table departments \
+--as-textfile \
+--target-dir=/user/datapath/datasets/departments \
+--delete-target-dir > /tmp/log_departments.log
+
+sqoop import \
+--connect "jdbc:mysql:///mysql:3306/retail_db" \
+--username=root \
+--password=root \
+--table categories \
+--as-textfile \
+--target-dir=/user/datapath/datasets/categories \
+--delete-target-dir > /tmp/log_categories.log
+
+sqoop import \
+--connect "jdbc:mysql://mysql:3306/retail_db" \
+--username=root \
+--password=root \
+--table orders \
+--as-textfile \
+--target-dir=/user/datapath/datasets/orders \
+--delete-target-dir > /tmp/log_orders.log
+
+# sqoop import \
+# --connect "jdbc:mysql://mysql:3306/bd_vanessa" \
+# --username=root \
+# --password=root \
+# --table student_mat \
+# --split-by age \
+# --as-textfile \
+# --target-dir=/user/raw/mysql/bd_vanessa/t_student_mat \
+# --delete-target-dir > /tmp/log_customer.log
